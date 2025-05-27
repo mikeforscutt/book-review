7d92ec5d149b23c6fee1deb98e13d76ec2e787e1
mikeforscutt <mikeforscutt@gmail.com>
2025-05-27 15:20:04 +0100

    Add create review functionality

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
@@ -17,17 +18,24 @@ class ReviewController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Book $book)
     {
-        //
+        return view('reviews.create', ['book' => $book]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Book $book)
     {
-        //
+        $data = $request->validate([
+            'review' => 'required|min:15',
+            'rating' => 'required|integer|min:1|max:5'
+        ]);
+
+        $book->reviews()->create($data);
+
+        return redirect()->route('books.show', $book)->with('success', 'Review added successfully');;
     }
 
     /**
